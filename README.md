55f2a1929177596df86b7ba72095169331dff1b5
Mr Chainman <mr.chainman@0box.eu>
2019-12-24 23:06:02 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PushPy
+A python script that fetches notifications from the nextcloud talk api and pushes them to a gotify server.
+
+
+### TODO
+* Currently the number of conversations must be specified manually. It would be cool to parse the number of conversations from the json data.
+* Handle Calls
+* Check how it works with group chats
+
---
+++ push_msg.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import requests
+import json
+import time
+
+# set nextcloud variables
+url = <https://cloud.example.com/ocs/v2.php/apps/spreed/api/v1/room>
+user = <username>
+pw = <password>
+# set header
+headers = {'OCS-APIRequest': 'true',
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+          }
+# create empty messages list
+messages = []
+
+# set gotify variables
+urlpush = <gotify-url>
+token = <gotify-app-token>
+headerspush = {'X-Gotify-Key': token}
+
+# start infinite loop for listening
+while True:
+    # retrieve json data from talk app
+    r = requests.get(url, headers=headers, auth=(user, pw))
+    m = (r.json())
+    # iterate through conversations, needs to be automatically fethed.
+    for i in 0,1,2:
+        who = m["ocs"]["data"][i]["lastMessage"]["actorDisplayName"]
+        msg = m["ocs"]["data"][i]["lastMessage"]["message"]
+        # check if message was already pushed
+        if msg in messages:
+            continue
+        # check if I wrote the message
+        elif who == "Your Name":
+            continue
+        # else send push notification
+        else:
+            requests.post(urlpush, headers=headerspush, data={'title': who, 'message': msg, 'priority': '10'})
+            messages.append(msg)
+        # wait 5 seconds
+    time.sleep( 5 )
