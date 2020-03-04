57a90d3e2c888e67ca10c2e0ccdb3bd2d6c8b44a
mrchainman <mr.chainman@0box.eu>
2020-03-04 16:30:08 +0100

    Update push_msg.py

--- push_msg.py
+++ push_msg.py
@@ -17,26 +17,27 @@ headerspush = {'X-Gotify-Key': token}
 messages = []
 
 # start infinite loop for listening
-while True:
-    # retrieve json data from talk app
-    r = requests.get(url, headers=headers, auth=(user, pw))
-    # load the json data
-    m = (r.json())
-    # Iterate through the conversations
-    for i in range(len(m["ocs"]["data"])):
-        # Parse who send the message
-        who = m["ocs"]["data"][i]["lastMessage"]["actorDisplayName"]
-        msg = m["ocs"]["data"][i]["lastMessage"]["message"]
-        # check if message was already pushed
-        if msg in messages:
-            continue
-        # check if I wrote the message
-        elif who == me:
-            continue
-        # else send push notification
-        else:
-            requests.post(urlpush, headers=headerspush, data={'title': who, 'message': msg, 'priority': '10'})
-            # Add the message to the list
-            messages.append(msg)
-    # wait 5 seconds
-    time.sleep( 5 )
+if __name__ == "__main__":
+           while True:
+                      # retrieve json data from talk app
+                      r = requests.get(url, headers=headers, auth=(user, pw))
+                      # load the json data
+                      m = (r.json())
+                      # Iterate through the conversations
+                      for i in range(len(m["ocs"]["data"])):
+                                 # Parse who send the message
+                                 who = m["ocs"]["data"][i]["lastMessage"]["actorDisplayName"]
+                                 msg = m["ocs"]["data"][i]["lastMessage"]["message"]
+                                 # check if message was already pushed
+                                 if msg in messages:
+                                            continue
+                                 # check if I wrote the message
+                                 elif who == me:
+                                            continue
+                                 # else send push notification
+                                 else:
+                                            requests.post(urlpush, headers=headerspush, data={'title': who, 'message': msg, 'priority': '10'})
+                                            # Add the message to the list
+                                            messages.append(msg)
+                      # wait 5 seconds
+                      time.sleep( 5 )
