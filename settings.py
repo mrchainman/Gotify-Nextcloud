3304406c40c3cad16538f96b6e4a2ce45e4dec78
mrchainman <mr.chainman@0box.eu>
2020-03-04 16:10:43 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Nextcloud settings
-url=''
-user=''
-pw=''
+url='https://cloud.example.com'
+user='username'
+pw='secretpw'
 # Gotify settings
-urlpush=''
-token=''
+urlpush='https://push.example.com'
+token='TOKEN'
 # Your name, used to prevent getting notifications for messages you wrote
-me=''
+me='John Smith'
