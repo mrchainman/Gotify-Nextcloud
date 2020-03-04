654da0e6ed3a06672bd86d3ce8c7d092cf0d63d8
mrchainman <mr.chainman@0box.eu>
2020-03-04 16:42:15 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,10 +5,10 @@ def test_messages_are_empty():
    assert len(messages) == 0
    
 def test_variables_not_empty():   
-   assert len(url) == 0
-   assert len(user) == 0
-   assert len(pw) == 0
-   assert len(urlpush) == 0
-   assert len(token) == 0
-   assert len(me) == 0
+   assert len(url) != 0
+   assert len(user) != 0
+   assert len(pw) != 0
+   assert len(urlpush) != 0
+   assert len(token) != 0
+   assert len(me) != 0
                   
